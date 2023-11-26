97eb047679fde452d6d90f4c135d61c91920e199
Daniel Ramirez <dani_0318@hotmail.com>
2023-11-26 00:30:58 -0500

    add countries module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'selector',
+    loadChildren: () => import('./countries/countries-routing.module').then( m => m.CountriesRoutingModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'selector'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
